81ee8f4c3d0c3c4971efa0a23d52345d7a1f3237
Mason <masoncquick@gmail.com>
2026-03-08 15:24:47 -0400

    yes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
